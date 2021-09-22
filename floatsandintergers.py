0a8a40a0d42a708d4f17408c0052c816ff2cf570
UgochukwuO <ugo.onwuzurike@yahoo.com>
2021-09-22 09:39:01 -0500

    Add files via upload

---
+++ floatsandintergers.py
@@ -0,0 +1,37 @@
+# difference between float and int 
+# Float = value + decimals ex 2.343, 4322.04 Int = whole number 5, 2, 343454535354, 
+## integers into float 
+
+# int(5) + 4.5 -> float
+
+#find the radius of a circle
+
+pi = 3.14 #float
+r = 3
+
+radiusOfACirlce = pi*r**2
+
+
+print(radiusOfACirlce)
+
+#what is a scenario in life that you would only use whole numbers?
+
+#hmm your age?
+
+# okay lets write a program that adds together to peoples ages
+
+age1 = 26
+age2 = 23
+
+sumOfAge = age1 + age2
+
+print("The sum of " + str(age1) + " and " + str(age2) + " is = " + str(sumOfAge))
+
+
+#this is an example where you only want to be using integers. 
+#if we run that it wont create a problem for us because we are adding 
+#integers and string? 
+
+#good catch, yes it will. but we can use fuction to convert integers to strings as well. one moment...
+
+#no problem, would that function be str()?!!!! YES
