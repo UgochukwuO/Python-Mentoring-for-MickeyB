b0fd7c4fdeea1524084cdde26d0452719ce393b6
UgochukwuO <ugo.onwuzurike@yahoo.com>
2021-09-22 09:39:49 -0500

    Add files via upload

---
+++ ifelsestatements.py
@@ -0,0 +1,23 @@
+
+num1 = 6
+num2 = 9
+
+if (num1 < num2):
+    print("true")
+else: 
+    print("false")
+
+
+
+#create variables
+creditScore = 700
+age = 25
+
+
+#if else statemt
+if (creditScore < 740): 
+    print("Loan Decline")
+else: 
+    print("Loan Approved")
+
+
