2b977cdbf56efae67cad2ebc7f809192521eac43
UgochukwuO <ugo.onwuzurike@yahoo.com>
2021-09-22 09:58:47 -0500

    Create introToArrays.py

---
+++ introToArrays.py
@@ -0,0 +1,32 @@
+# intro to arrays
+# arrays must be of the same data type. 
+# Ex: Array of Strings. studentsNames = ["John", "Sam", "Rob"]
+# Ex: Array of ints. classesScores = [86, 74, 56]
+
+classesScores = [86, 74, 56] # This is defined as an array of elements or a list of elements
+
+# You can access elements in an array with there index.
+# Array indexing start from the 0th index for the 1st element of the array.
+
+print(claseScores[0]) 
+# output = 86
+
+print(claseScores[1]) 
+# output = 74
+
+print(claseScores[2]) 
+# output = 56
+
+
+
+studentsNames = ["John", "Sam", "Rob"]
+
+print(studentsNames[0]) 
+# output = John
+
+print(studentsNames[1]) 
+# output = Sam
+
+print(studentsNames[2]) 
+# output = Rob
+
